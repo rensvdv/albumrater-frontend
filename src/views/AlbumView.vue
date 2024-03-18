e4dc4f0729ce2153e3c591f0be1c8355a75e58b9
Rens van der Velden <I491926@fhict.nl>
2024-03-18 15:21:17 +0100

    Albums ophalen uit back-end toegevoegd

---
+++ src/views/AlbumView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="albums">
+    <div v-for="album in albums" :key="album.id">
+      <h2>{{ album.id }} {{ album.title }}</h2>
+      <p>{{ album.artist }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "AlbumView",
+  data() {
+    return {
+      albums: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:8080/api/albums')
+        .then((response) => {
+          this.albums = response.data
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
